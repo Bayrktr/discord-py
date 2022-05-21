6934ab4966a5c63e3e46499590b7ca3ee5390ea9
Emir <96651996+Bayrktr@users.noreply.github.com>
2022-05-22 02:04:04 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+rutin = 1000
+
 
 def connectWebSite(ZodiacSignName):
     def webSiteCheck(ZodiacSignName):
@@ -19,9 +21,55 @@ def connectWebSite(ZodiacSignName):
         return "Servis Kullanım Dışı"
 
 
-def takeZodiacSignName():
-    print()
+def gameNews():
+    def link():
+        links = []
+        a = requests.get("https://shiftdelete.net/oyun")
+        a = a.text
+        soup = BeautifulSoup(a, "html.parser")
+        value = soup.find("div", attrs={"class": "sidebar-content-main"})
+        value = soup.find_all("a", value, href=True)
+        for x in value:
+            links.append(x["href"])
+        links = links[18]
+        return links
 
+    def newsContent():
+        def pictureLink():
+            pictureValue = soup.find_all("img", Mainvalue)
+            for x in pictureValue:
+                print(x["src"])
+            pictureValue = pictureValue[5]["src"]
+            return pictureValue
 
-ZodiacSignName = "terazi"
-connectWebSite(ZodiacSignName)
+        contentList = []
+        a = requests.get(link())
+        a = a.text
+        soup = BeautifulSoup(a, "html.parser")
+        Mainvalue = soup.find("div", attrs={"class": "sidebar-content-main"})
+
+        value = soup.find_all("p", Mainvalue)
+        for x in value:
+            if x.text == "":
+                break
+            else:
+                contentList.append(x.text)
+
+        # 2000 word check
+
+        wordNumber = 2 + 8
+        wordNumber -= len(contentList[len(contentList) - 1])
+        title = contentList[0]
+        contentList.pop(0)
+        newContentList = []
+        wordNumber += len(title)
+        for x in contentList:
+            if wordNumber + len(x) < 2000:
+                newContentList.append(x)
+            else:
+                break
+        newContentList.pop()
+        newContentList = "".join(newContentList)
+        return title, newContentList, pictureLink()
+
+    return newsContent()
